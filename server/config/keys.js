b531d07c5937ff5505d67ed5e52734fbce2eaae9
serjt12 <jandreus87@hotmail.com>
2018-06-10 08:03:44 -0500

    mongodb set up with

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://bears-team:bearsteam6@ds153890.mlab.com:53890/bears6'
+}
--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+require('./models/User')
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 app.use(bodyParser.json());
