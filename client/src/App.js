9b012ac434ab946fd7c3c8b137160c2544e7db0a
Aaron <aaronmassey45@gmail.com>
2018-06-11 09:14:59 -0500

    Get's grade level of this.state.text

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
-  componentDidMount() {}
+  state = {
+    gradeLevel: '',
+    text:
+      'I will build a great, great wall on our southern border and I will have Mexico pay for that wall.',
+  };
+
+  componentDidMount() {
+    axios
+      .post('/api/readability', {
+        text: this.state.text,
+      })
+      .then(res => this.setState({ ...res.data }));
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Basic Setup</h1>
+        <h3>
+          The grade level of "{this.state.text}" is: {this.state.gradeLevel}
+        </h3>
       </div>
     );
   }
