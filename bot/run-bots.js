c6d5b4ce0ac29beaed79a13a735cbb563dad1e4f
Faxn <blake.curry@gmx.us>
2018-06-11 13:18:27 -0400

    Improved subprocess spawning. Useing spawn instead of exec to prevent a somewhat unlikley command injection.

--- bot/run-bots.js
+++ bot/run-bots.js
@@ -1,19 +1,22 @@
 const child_process = require('child_process');
-//require('dotenv').config()
 
 process.env.PATH += ":node_modules/.bin";
+
+if (!process.env.hasOwnProperty("HUBOT_ADAPTERS")){
+    console.log("HUBOT_ADAPTERS not set, defaulting to slack");
+    process.env.HUBOT_ADAPTERS='slack';
+}
 const adapters = process.env.HUBOT_ADAPTERS.split(":");
 
 bots = {};
 for (const adapter of adapters){
-    bots[adapter] = child_process.exec('hubot --adapter '+adapter);
-    
-}
-
-for(const [adapter, botProc] of Object.entries(bots)){
-    botProc.stdout.on('data', function(chunk){
-        console.log(adapter +" Bot: "+ chunk.toString());
+    if (adapter == "") continue;
+    bots[adapter] = child_process.spawn('hubot', ['--disable-httpd', '--adapter', adapter]);
+    bots[adapter].stdout.on('data', function(chunk){
+        process.stdin.write(adapter +" Bot: "+ chunk.toString());
+    });
+    bots[adapter].stderr.on('data', function(chunk){
+        process.stderr.write(adapter +" Bot: "+ chunk.toString());
     });
 }
-
 
