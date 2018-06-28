ae37367af6b3c04099ea2cc40c27306bc36aebfa
Faxn <blake.curry@gmx.us>
2018-06-28 12:53:24 -0400

    Made server check for existence of client files if not running in development mode

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('./models/User');
+const fs = require('fs')
 
 const User = mongoose.model('users')
 
@@ -19,7 +20,12 @@ app.get('/test', (req, res) => res.send('Hello from the server.'));
 // If we aren't in development then host the built client files in our node instance.
 var env = process.env.NODE_ENV;
 if (env != 'development'){
+  if(! fs.existsSync('../client/build')){
+    process.stderr.write("'../client/build' missing. Make sure the client files are getting built or run in with NODE_ENV=development\n")
+    process.exit(1)
+  }
   app.use(express.static('../client/build'));
+  
 }
 
 
