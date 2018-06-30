8593c5d41528523da518e567649fc7baf053231c
serjt12 <jandreus87@hotmail.com>
2018-06-30 11:52:45 -0500

    user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema ({
-  userName: String
+const userSchema = new Schema({
+  userName: String,
+  Messages: [{
+    body: String,
+    sent: Date,
+    analysis: [{processor: String, processed: Date, result: String}]
+  }],
+  storeMessages: Boolean,
+  showStats: Boolean
 })
 
 mongoose.model('users', userSchema);
